e6cdcb1c1c938c0354a1059ecdf80dca4fb1e7d9
Denis Miller <danism76@gmail.com>
2026-08-16 22:23:34 +0600

    chore: initialize JavaScript learning project

---
+++ 01-variables.js
@@ -0,0 +1,7 @@
+const testerName = 'Danis';
+const learningHoursPerWeek = 20;
+const isLearningPlaywright = true;
+
+console.log(`Тестровщик ${testerName}`);
+console.log(`Часов обучения в неделю: ${learningHoursPerWeek}`);
+console.log(`Изучаю Playwright: ${isLearningPlaywright}`);
